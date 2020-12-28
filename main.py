f0dcaad489539aaa7902cc5ec16e30f90ee754dd
Prkrr20 <230140@lasallehs.net>
2020-12-28 01:13:00 +0000

    Infdev 1.1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 import math
 from perlin_noise import PerlinNoise
-import matplotlib.pyplot as plt
+import replit
 
 noise = PerlinNoise(octaves=3, seed=random.randint(0, 999999999))
 
@@ -10,10 +10,6 @@ pic = [noise(i/width) for i in range(width)]
 lowest = min(pic)
 print(lowest)
 pic1 = [math.ceil((noise(i/width)+(abs(lowest)))*10) for i in range(width)]
-
-
-for i in range(width):
-	plt.plot(i, pic1[i], 'go')
 
 g10 = ""
 g9 = ""
@@ -143,7 +139,7 @@ ground = [g10,g9,g8,g7,g6,g5,g4,g3,g2,g1]
 
 py = 0
 px = 0
-
+pdir = 1
 for i in range(len(ground)):
 	if (ground[i][0]=="#"):
 		py = i-1
@@ -157,41 +153,71 @@ for i in range(len(ground)):
 
 Game = True
 
+def mine(dr):
+	if (ground[py][px+dr]=="#"):
+		ground[py] = ground[py][:px+dr] + " " + ground[py][px+dr+1:]
+	elif (ground[py][px+dr]==" "):
+		if (ground[py+1][px+dr]=="#"):
+			ground[py+1] = ground[py+1][:px+dr] + " " + ground[py+1][px+dr+1:]
+
+def place(dr):
+	if (ground[py][px+dr]==" "):
+		if (ground[py+1][px+dr]==" "):
+			ground[py+1] = ground[py+1][:px+dr] + "#" + ground[py+1][px+dr+1:]
+		else:
+			ground[py] = ground[py][:px+dr] + "#" + ground[py][px+dr+1:]
+	
+
+
 def move(dr):
 	global px
 	global py
-	if (ground[py][px+dr]==" "):
-		ground[py] = ground[py][:px+dr] + "o" + ground[py][px+dr+1:]
-		ground[py] = ground[py][:px] + " " + ground[py][px+1:]
-		px+=dr
-		flyCheck = True
-		while flyCheck:
-			if (ground[py+1][px]=="#"):
-				flyCheck = False
-				break
-			ground[py+1] = ground[py+1][:px] + "o" + ground[py+1][px+1:]
-			ground[py] = ground[py][:px] + " " + ground[py][px+1:]
-			py+=1
-	elif (ground[py-1][px+dr]==" "):
-		ground[py-1] = ground[py-1][:px+dr] + "o" + ground[py-1][px+dr+1:]
-		ground[py] = ground[py][:px] + " " + ground[py][px+1:]
-		py-=1
-		px+=dr
+	global pdir
+	pdir = dr
+	if not (px == 0 and dr == -1):
+		if not (px == 74 and dr == 1):
+			if (ground[py][px+dr]==" "):
+				ground[py] = ground[py][:px+dr] + "o" + ground[py][px+dr+1:]
+				ground[py] = ground[py][:px] + " " + ground[py][px+1:]
+				px+=dr
+				flyCheck = True
+				while flyCheck:
+					if (ground[py+1][px]=="#") or (py == 9):
+						flyCheck = False
+						break
+					ground[py+1] = ground[py+1][:px] + "o" + ground[py+1][px+1:]
+					ground[py] = ground[py][:px] + " " + ground[py][px+1:]
+					py+=1
+			elif (ground[py-1][px+dr]==" "):
+				ground[py-1] = ground[py-1][:px+dr] + "o" + ground[py-1][px+dr+1:]
+				ground[py] = ground[py][:px] + " " + ground[py][px+1:]
+				py-=1
+				px+=dr
 
 
 def reg(cmd):
 	global Game
+	global pdir
+	global px
+	global py
 	if (cmd == "exit()"):
 		Game = False
 	elif (cmd == "d"):
+		pdir = 1
 		move(1)
 	elif (cmd == "a"):
+		pdir = -1
 		move(-1)
+	elif (cmd == "x"):
+		mine(pdir)
+	elif (cmd == "p"):
+		place(pdir)
 
 
 
 
 while Game:
+	replit.clear()
 	for i in ground:
 		print(i)
 	inp = input()
