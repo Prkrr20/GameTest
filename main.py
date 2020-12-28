1feb240c3d750ec8029ad4b5a73a1f72ab9c38f4
Prkrr20 <230140@lasallehs.net>
2020-12-28 01:34:17 +0000

    Infdev 1.1.1

--- main.py
+++ main.py
@@ -3,139 +3,159 @@ import math
 from perlin_noise import PerlinNoise
 import replit
 
-noise = PerlinNoise(octaves=3, seed=random.randint(0, 999999999))
+print("Game Thing py Prkrr20\n\n")
+print("1. Perlin Generated")
+print("2. Superflat")
+menu = input()
+ground = []
+if (menu == "1"):
+	noise = PerlinNoise(octaves=3, seed=random.randint(0, 999999999))
 
-width = 75
-pic = [noise(i/width) for i in range(width)]
-lowest = min(pic)
-print(lowest)
-pic1 = [math.ceil((noise(i/width)+(abs(lowest)))*10) for i in range(width)]
+	width = 75
+	pic = [noise(i/width) for i in range(width)]
+	lowest = min(pic)
+	print(lowest)
+	pic1 = [math.ceil((noise(i/width)+(abs(lowest)))*10) for i in range(width)]
 
-g10 = ""
-g9 = ""
-g8 = ""
-g7 = ""
-g6 = ""
-g5 = ""
-g4 = ""
-g3 = ""
-g2 = ""
-g1 = ""
+	g10 = ""
+	g9 = ""
+	g8 = ""
+	g7 = ""
+	g6 = ""
+	g5 = ""
+	g4 = ""
+	g3 = ""
+	g2 = ""
+	g1 = ""
 
 
-for i in pic1:
-	if (i == 0):
-		g1 += "#"
-		g2 += " "
-		g3 += " "
-		g4 += " "
-		g5 += " "
-		g6 += " "
-		g7 += " "
-		g8 += " "
-		g9 += " "
-		g10 += " "
-	elif (i == 1):
-		g1 += "#"
-		g2 += "#"
-		g3 += " "
-		g4 += " "
-		g5 += " "
-		g6 += " "
-		g7 += " "
-		g8 += " "
-		g9 += " "
-		g10 += " "
-	elif (i == 2):
-		g1 += "#"
-		g2 += "#"
-		g3 += "#"
-		g4 += " "
-		g5 += " "
-		g6 += " "
-		g7 += " "
-		g8 += " "
-		g9 += " "
-		g10 += " "
-	elif (i == 3):
-		g1 += "#"
-		g2 += "#"
-		g3 += "#"
-		g4 += "#"
-		g5 += " "
-		g6 += " "
-		g7 += " "
-		g8 += " "
-		g9 += " "
-		g10 += " "
-	elif (i == 4):
-		g1 += "#"
-		g2 += "#"
-		g3 += "#"
-		g4 += "#"
-		g5 += "#"
-		g6 += " "
-		g7 += " "
-		g8 += " "
-		g9 += " "
-		g10 += " "
-	elif (i == 5):
-		g1 += "#"
-		g2 += "#"
-		g3 += "#"
-		g4 += "#"
-		g5 += "#"
-		g6 += "#"
-		g7 += " "
-		g8 += " "
-		g9 += " "
-		g10 += " "
-	elif (i == 6):
-		g1 += "#"
-		g2 += "#"
-		g3 += "#"
-		g4 += "#"
-		g5 += "#"
-		g6 += "#"
-		g7 += "#"
-		g8 += " "
-		g9 += " "
-		g10 += " "
-	elif (i == 7):
-		g1 += "#"
-		g2 += "#"
-		g3 += "#"
-		g4 += "#"
-		g5 += "#"
-		g6 += "#"
-		g7 += "#"
-		g8 += "#"
-		g9 += " "
-		g10 += " "
-	elif (i == 8):
-		g1 += "#"
-		g2 += "#"
-		g3 += "#"
-		g4 += "#"
-		g5 += "#"
-		g6 += "#"
-		g7 += "#"
-		g8 += "#"
-		g9 += "#"
-		g10 += " "
-	elif (i == 9):
-		g1 += "#"
-		g2 += "#"
-		g3 += "#"
-		g4 += "#"
-		g5 += "#"
-		g6 += "#"
-		g7 += "#"
-		g8 += "#"
-		g9 += "#"
-		g10 += "#"
+	for i in pic1:
+		if (i == 0):
+			g1 += "B"
+			g2 += " "
+			g3 += " "
+			g4 += " "
+			g5 += " "
+			g6 += " "
+			g7 += " "
+			g8 += " "
+			g9 += " "
+			g10 += " "
+		elif (i == 1):
+			g1 += "B"
+			g2 += "#"
+			g3 += " "
+			g4 += " "
+			g5 += " "
+			g6 += " "
+			g7 += " "
+			g8 += " "
+			g9 += " "
+			g10 += " "
+		elif (i == 2):
+			g1 += "B"
+			g2 += "#"
+			g3 += "#"
+			g4 += " "
+			g5 += " "
+			g6 += " "
+			g7 += " "
+			g8 += " "
+			g9 += " "
+			g10 += " "
+		elif (i == 3):
+			g1 += "B"
+			g2 += "#"
+			g3 += "#"
+			g4 += "#"
+			g5 += " "
+			g6 += " "
+			g7 += " "
+			g8 += " "
+			g9 += " "
+			g10 += " "
+		elif (i == 4):
+			g1 += "B"
+			g2 += "#"
+			g3 += "#"
+			g4 += "#"
+			g5 += "#"
+			g6 += " "
+			g7 += " "
+			g8 += " "
+			g9 += " "
+			g10 += " "
+		elif (i == 5):
+			g1 += "B"
+			g2 += "#"
+			g3 += "#"
+			g4 += "#"
+			g5 += "#"
+			g6 += "#"
+			g7 += " "
+			g8 += " "
+			g9 += " "
+			g10 += " "
+		elif (i == 6):
+			g1 += "B"
+			g2 += "#"
+			g3 += "#"
+			g4 += "#"
+			g5 += "#"
+			g6 += "#"
+			g7 += "#"
+			g8 += " "
+			g9 += " "
+			g10 += " "
+		elif (i == 7):
+			g1 += "B"
+			g2 += "#"
+			g3 += "#"
+			g4 += "#"
+			g5 += "#"
+			g6 += "#"
+			g7 += "#"
+			g8 += "#"
+			g9 += " "
+			g10 += " "
+		elif (i == 8):
+			g1 += "B"
+			g2 += "#"
+			g3 += "#"
+			g4 += "#"
+			g5 += "#"
+			g6 += "#"
+			g7 += "#"
+			g8 += "#"
+			g9 += "#"
+			g10 += " "
+		elif (i == 9):
+			g1 += "B"
+			g2 += "#"
+			g3 += "#"
+			g4 += "#"
+			g5 += "#"
+			g6 += "#"
+			g7 += "#"
+			g8 += "#"
+			g9 += "#"
+			g10 += "#"
 
-ground = [g10,g9,g8,g7,g6,g5,g4,g3,g2,g1]
+	ground = [g10,g9,g8,g7,g6,g5,g4,g3,g2,g1]
+elif (menu == "2"):
+	g1 = "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"
+	g2 = "###########################################################################"
+	g3 = "###########################################################################"
+	g4 = "                                                                           "
+	g5 = "                                                                           "
+	g6 = "                                                                           "
+	g7 = "                                                                           "
+	g8 = "                                                                           "
+	g9 = "                                                                           "
+	g10 = "                                                                           "
+	ground = [g10,g9,g8,g7,g6,g5,g4,g3,g2,g1]
+else:
+	print("something went wrong. please try again later")
 
 py = 0
 px = 0
@@ -182,7 +202,7 @@ def move(dr):
 				px+=dr
 				flyCheck = True
 				while flyCheck:
-					if (ground[py+1][px]=="#") or (py == 9):
+					if (ground[py+1][px]!=" "):
 						flyCheck = False
 						break
 					ground[py+1] = ground[py+1][:px] + "o" + ground[py+1][px+1:]
